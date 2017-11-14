8004d415097b485d3800b7e2990b74d6e463fc2d
Tiago Madeira <negativespade@gmail.com>
2017-11-14 17:06:06 +0000

    Controls changed, prespective mode and some code and comment cleaned TODOS

--- MicroMouse/MicroMouse.js
+++ MicroMouse/MicroMouse.js
@@ -1,123 +1,80 @@
-//////////////////////////////////////////////////////////////////////////////
-//
-//  WebGL_example_24_GPU_per_vertex.js
-//
-//  Phong Illumination Model on the GPU - Per vertex shading - Several light sources
-//
-//  Reference: E. Angel examples
-//
-//  J. Madeira - November 2017
-//
-//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+//																//
+//  Tiago Madeira 76321 & Diogo Duarte 77645 - November 2017	//
+//																//
+//////////////////////////////////////////////////////////////////
 
 
-//----------------------------------------------------------------------------
+//----------------------------------------------------------------
 //
 // Global Variables
 //
 
-var gl = null; // WebGL context
+// WebGL context
+var gl = null; 
 
 var shaderProgram = null;
-
 var triangleVertexPositionBuffer = null;
-
-var triangleVertexNormalBuffer = null;		//NEW
+var triangleVertexNormalBuffer = null;
 
 // The GLOBAL transformation parameters
-
 var globalAngleYY = 0.0;
-
 var globalTz = 0.0;
 
 // The local transformation parameters
 
 // The translation vector
-
 var tx = 0.0;
-
 var ty = 0.0;
-
 var tz = 0.0;
 
 // The rotation angles in degrees
-
 var angleXX = 0.0;
-
 var angleYY = 0.0;
-
 var angleZZ = 0.0;
 
 // The scaling factors
-
 var sx = 1;
-
 var sy = 1;
-
 var sz = 1;
 
-// GLOBAL Animation controls
-
+// Global Animation controls
 var globalRotationYY_ON = 1;
-
 var globalRotationYY_DIR = 1;
-
 var globalRotationYY_SPEED = 1;
 
 // Local Animation controls
-
 var rotationXX_ON = 1;
-
 var rotationXX_DIR = 1;
-
 var rotationXX_SPEED = 1;
-
 var rotationYY_ON = 1;
-
 var rotationYY_DIR = 1;
-
 var rotationYY_SPEED = 1;
-
 var rotationZZ_ON = 1;
-
 var rotationZZ_DIR = 1;
-
 var rotationZZ_SPEED = 1;
 
 // To allow choosing the way of drawing the model triangles
-
 var primitiveType = null;
 
-// To allow choosing the projection type
-
-var projectionType = 0;
-
-// NEW --- The viewer position
-
-// It has to be updated according to the projection type
-
+// The viewer position
 var pos_Viewer = [ 0.0, 0.0, 0.0, 1.0 ];
 
-// NEW --- Model Material Features
+// Model Material Features
 
 // Ambient coef.
-
 var kAmbi = [ 0.2, 0.2, 0.2 ];
 
 // Diffuse coef.
-
 var kDiff = [ 0.7, 0.7, 0.7 ];
 
 // Specular coef.
-
 var kSpec = [ 0.7, 0.7, 0.7 ];
 
 // Phong coef.
-
 var nPhong = 100;
 
 // Initial model has just ONE TRIANGLE
-
 var vertices = [
 
 		// FRONTAL TRIANGLE
@@ -143,20 +100,16 @@ var normals = [
 
 //----------------------------------------------------------------------------
 //
-// NEW - To count the number of frames per second (fps)
+// Count the number of frames per second (fps)
 //
 
 var elapsedTime = 0;
-
 var frameCount = 0;
-
 var lastfpsTime = new Date().getTime();;
-
 
 function countFrames() {
 
    var now = new Date().getTime();
-
    frameCount++;
 
    elapsedTime += (now - lastfpsTime);
@@ -191,7 +144,6 @@ function countFrames() {
 function initBuffers() {
 
 	// Vertex Coordinates
-
 	triangleVertexPositionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
@@ -199,13 +151,11 @@ function initBuffers() {
 	triangleVertexPositionBuffer.numItems = vertices.length / 3;
 
 	// Associating to the vertex shader
-
 	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
 			triangleVertexPositionBuffer.itemSize,
 			gl.FLOAT, false, 0, 0);
 
 	// Vertex Normal Vectors
-
 	triangleVertexNormalBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexNormalBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
@@ -213,7 +163,6 @@ function initBuffers() {
 	triangleVertexNormalBuffer.numItems = normals.length / 3;
 
 	// Associating to the vertex shader
-
 	gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute,
 			triangleVertexNormalBuffer.itemSize,
 			gl.FLOAT, false, 0, 0);
@@ -229,12 +178,9 @@ function drawModel( angleXX, angleYY, angleZZ,
 					mvMatrix,
 					primitiveType ) {
 
-	// The the global model transformation is an input
-
+	// The global model transformation is an input
 	// Concatenate with the particular model transformations
-
-    // Pay attention to transformation order !!
-
+    // Transformation order is important
 	mvMatrix = mult( mvMatrix, translationMatrix( tx, ty, tz ) );
 
 	mvMatrix = mult( mvMatrix, rotationZZMatrix( angleZZ ) );
@@ -246,21 +192,17 @@ function drawModel( angleXX, angleYY, angleZZ,
 	mvMatrix = mult( mvMatrix, scalingMatrix( sx, sy, sz ) );
 
 	// Passing the Model View Matrix to apply the current transformation
-
 	var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 
 	gl.uniformMatrix4fv(mvUniform, false, new Float32Array(flatten(mvMatrix)));
 
 	// Associating the data to the vertex shader
-
 	// This can be done in a better way !!
-
 	// Vertex Coordinates and Vertex Normal Vectors
 
 	initBuffers();
 
 	// Material properties
-
 	gl.uniform3fv( gl.getUniformLocation(shaderProgram, "k_ambient"),
 		flatten(kAmbi) );
 
@@ -274,14 +216,12 @@ function drawModel( angleXX, angleYY, angleZZ,
 		nPhong );
 
     // Light Sources
-
 	var numLights = 2;
 
 	gl.uniform1i( gl.getUniformLocation(shaderProgram, "numLights"),
 		numLights );
 
 	//Light Sources
-
 	for(var i = 0; i < lightSources.length; i++ )
 	{
 		gl.uniform4fv( gl.getUniformLocation(shaderProgram, "allLights[" + String(i) + "].position"),
@@ -292,28 +232,21 @@ function drawModel( angleXX, angleYY, angleZZ,
     }
 
 	// Drawing
-
 	// primitiveType allows drawing as filled triangles / wireframe / vertices
 
 	if( primitiveType == gl.LINE_LOOP ) {
 
 		// To simulate wireframe drawing!
-
 		// No faces are defined! There are no hidden lines!
-
 		// Taking the vertices 3 by 3 and drawing a LINE_LOOP
 
 		var i;
-
 		for( i = 0; i < triangleVertexPositionBuffer.numItems / 3; i++ ) {
-
 			gl.drawArrays( primitiveType, 3 * i, 3 );
 		}
 	}
 	else {
-
 		gl.drawArrays(primitiveType, 0, triangleVertexPositionBuffer.numItems);
-
 	}
 }
 
@@ -324,79 +257,40 @@ function drawModel( angleXX, angleYY, angleZZ,
 function drawScene() {
 
 	var pMatrix;
-
 	var mvMatrix = mat4();
 
 	// Clearing the frame-buffer and the depth-buffer
-
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	// Computing the Projection Matrix
+	// A standard view volume.
+	// Viewer is at (0,0,0)
+	// Ensure that the model is "inside" the view volume
+	pMatrix = perspective( 45, 1, 0.05, 15 );
 
-	if( projectionType == 0 ) {
+	// Global transformation
+	globalTz = -2.5;
 
-		// For now, the default orthogonal view volume
+	//The viewer is on (0,0,0)
+	pos_Viewer[0] = pos_Viewer[1] = pos_Viewer[2] = 0.0;
+	pos_Viewer[3] = 1.0;
 
-		pMatrix = ortho( -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 );
-
-		// Global transformation !!
-
-		globalTz = 0.0;
-
-		// NEW --- The viewer is on the ZZ axis at an indefinite distance
-
-		pos_Viewer[0] = pos_Viewer[1] = pos_Viewer[3] = 0.0;
-
-		pos_Viewer[2] = 1.0;
-
-		// TO BE DONE !
-
-		// Allow the user to control the size of the view volume
-	}
-	else {
-
-		// A standard view volume.
-
-		// Viewer is at (0,0,0)
-
-		// Ensure that the model is "inside" the view volume
-
-		pMatrix = perspective( 45, 1, 0.05, 15 );
-
-		// Global transformation !!
-
-		globalTz = -2.5;
-
-		// NEW --- The viewer is on (0,0,0)
-
-		pos_Viewer[0] = pos_Viewer[1] = pos_Viewer[2] = 0.0;
-
-		pos_Viewer[3] = 1.0;
-
-		// TO BE DONE !
-
-		// Allow the user to control the size of the view volume
-	}
+	// TODO - Allow the user to control the size of the view volume
 
 	// Passing the Projection Matrix to apply the current projection
-
 	var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-
 	gl.uniformMatrix4fv(pUniform, false, new Float32Array(flatten(pMatrix)));
 
-	// NEW --- Passing the viewer position to the vertex shader
-
+	// Passing the viewer position to the vertex shader
 	gl.uniform4fv( gl.getUniformLocation(shaderProgram, "viewerPosition"),
         flatten(pos_Viewer) );
 
 	// GLOBAL TRANSFORMATION FOR THE WHOLE SCENE
-
 	mvMatrix = translationMatrix( 0, 0, globalTz );
 
-	// NEW - Updating the position of the light sources, if required
+	// Updating the position of the light sources, if required
 
-	// FOR EACH LIGHT SOURCE
-
+	// for each light source
 	for(var i = 0; i < lightSources.length; i++ )
 	{
 		if( lightSources[i].isOff() ) {
@@ -408,7 +302,7 @@ function drawScene() {
 
 		var lightSourceMatrix = mat4();
 
-		// COMPLETE THE CODE FOR THE OTHER ROTATION AXES
+		// TODO COMPLETE THE CODE FOR THE OTHER ROTATION AXES
 
 		if( lightSources[i].isRotYYOn() )
 		{
@@ -417,32 +311,28 @@ function drawScene() {
 					rotationYYMatrix( lightSources[i].getRotAngleYY() ) );
 		}
 
-		// NEW Passing the Light Souree Matrix to apply
-
+		// Passing the Light Souree Matrix to apply
 		var lsmUniform = gl.getUniformLocation(shaderProgram, "allLights["+ String(i) + "].lightSourceMatrix");
-
 		gl.uniformMatrix4fv(lsmUniform, false, new Float32Array(flatten(lightSourceMatrix)));
 	}
 
 	// Instantianting the current model
-
 	drawModel( angleXX, angleYY, angleZZ,
 	           sx, sy, sz,
 	           tx, ty, tz,
 	           mvMatrix,
 	           primitiveType );
 
-	// NEW - Counting the frames
-
+	// Counting the frames
 	countFrames();
 }
 
 //----------------------------------------------------------------------------
 //
-//  NEW --- Animation
+// Animation
 //
 
-// Animation --- Updating transformation parameters
+// Updating transformation parameters
 
 var lastTime = 0;
 
@@ -546,35 +436,47 @@ function outputInfos(){
 
 // Handling keyboard events
 
-// Adapted from www.learningwebgl.com
-
 var currentlyPressedKeys = {};
 
 function handleKeys() {
 
+	// Page Up
 	if (currentlyPressedKeys[33]) {
-
-		// Page Up
+		//Zoom out
 		sx *= 0.9;
-
 		sz = sy = sx;
 	}
+	// Page Down
 	if (currentlyPressedKeys[34]) {
-
-		// Page Down
-
+		//Zoom in
 		sx *= 1.1;
-
 		sz = sy = sx;
+	}
+	// W or w
+	if (currentlyPressedKeys[87] || currentlyPressedKeys[119]) {
+		// go up
+		ty += 0.01;
+	}
+	// A or a
+	if (currentlyPressedKeys[65] || currentlyPressedKeys[97]) {
+		// go left
+		tx -= 0.01;
+	}
+	// S or s
+	if (currentlyPressedKeys[83] || currentlyPressedKeys[115]) {
+		// go down
+		ty -= 0.01;
+	}
+	// D or d
+	if (currentlyPressedKeys[68] || currentlyPressedKeys[100]) {
+		// go right
+		tx += 0.01;
 	}
 }
 
 //----------------------------------------------------------------------------
 
 // Handling mouse events
-
-// Adapted from www.learningwebgl.com
-
 
 var mouseDown = false;
 
@@ -625,129 +527,67 @@ function handleMouseMove(event) {
 //----------------------------------------------------------------------------
 
 function setEventListeners( canvas ){
-
     // Mouse + Keyboard Events
-	// From learningwebgl.com
-
     canvas.onmousedown = handleMouseDown;
-
     document.onmouseup = handleMouseUp;
-
     document.onmousemove = handleMouseMove;
-
-    // NEW ---Handling the keyboard
-
-	// From learningwebgl.com
-
+    // Handling the keyboard
     function handleKeyDown(event) {
-
         currentlyPressedKeys[event.keyCode] = true;
     }
-
     function handleKeyUp(event) {
-
         currentlyPressedKeys[event.keyCode] = false;
     }
-
 	document.onkeydown = handleKeyDown;
-
     document.onkeyup = handleKeyUp;
 
-
-	// Adapted from:
-
 	// http://stackoverflow.com/questions/23331546/how-to-use-javascript-to-read-local-text-file-and-read-line-by-line
-
 	document.getElementById("file").onchange = function(){
-
 		var file = this.files[0];
-
 		var reader = new FileReader();
-
 		reader.onload = function( progressEvent ){
-
 			// Entire file read as a string
-
 			// The tokens/values in the file
 
 			// Separation between values is 1 or mode whitespaces
-
 			var tokens = this.result.split(/\s\s*/);
 
 			// Array of values; each value is a string
-
 			var numVertices = parseInt( tokens[0] );
 
 			// For every vertex we have 3 floating point values
-
 			var i, j;
-
 			var aux = 1;
-
 			var newVertices = [];
 
 			for( i = 0; i < numVertices; i++ ) {
-
 				for( j = 0; j < 3; j++ ) {
-
 					newVertices[ 3 * i + j ] = parseFloat( tokens[ aux++ ] );
 				}
 			}
-
 			// Assigning to the current model
-
 			vertices = newVertices.slice();
 
-			// NEW --- Computing the triangle normal vector for every vertex
-
+			//Computing the triangle normal vector for every vertex
 			computeVertexNormals( vertices, normals );
 
 			// To render the model just read
-
 			initBuffers();
 
-			// RESET the transformations - NEED AUXILIARY FUNCTION !!
-
+			// Reset the transformations
 			tx = ty = tz = 0.0;
-
 			angleXX = angleYY = angleZZ = 0.0;
-
 			sx = sy = sz = 1;
 		};
 
 		// Entire file read as a string
-
 		reader.readAsText( file );
 	}
-
-    // Dropdown list
-
-	var projection = document.getElementById("projection-selection");
-
-	projection.addEventListener("click", function(){
-
-		// Getting the selection
-
-		var p = projection.selectedIndex;
-
-		switch(p){
-
-			case 0 : projectionType = 0;
-				break;
-
-			case 1 : projectionType = 1;
-				break;
-		}
-	});
-
 	// Dropdown list
-
 	var list = document.getElementById("rendering-mode-selection");
-
 	list.addEventListener("click", function(){
 
 		// Getting the selection
-
 		var mode = list.selectedIndex;
 
 		switch(mode){
@@ -803,38 +643,6 @@ function setEventListeners( canvas ){
 
 		rotationZZ_SPEED = 1;
 	};
-
-	document.addEventListener("keypress", function(event){
-
-		var key = event.charCode; // ASCII
-
-		switch(key){
-			//Go up
-			case 87 : // W
-			case 119: // w
-				ty += 0.01;
-				drawScene();
-				break;
-			//Go left
-			case 65: // A
-			case 97: // a
-				tx -= 0.01;
-				drawScene();
-				break;
-			//Go down
-			case 83: // S
-			case 115: // s
-				ty -= 0.01;
-				drawScene();
-				break;
-			//Go right
-			case 68: // D
-			case 100: // d
-				tx += 0.01;
-				drawScene();
-				break;
-		}
-	});
 }
 
 //----------------------------------------------------------------------------
@@ -846,33 +654,23 @@ function initWebGL( canvas ) {
 	try {
 
 		// Create the WebGL context
-
-		// Some browsers still need "experimental-webgl"
-
 		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 
 		// DEFAULT: The viewport occupies the whole canvas
 
 		// DEFAULT: The viewport background color is WHITE
 
-		// NEW - Drawing the triangles defining the model
-
+		// Drawing the triangles defining the model
 		primitiveType = gl.TRIANGLES;
 
 		// DEFAULT: Face culling is DISABLED
 
 		// Enable FACE CULLING
-
 		gl.enable( gl.CULL_FACE );
 
-		// DEFAULT: The BACK FACE is culled!!
-
-		// The next instruction is not needed...
-
-		gl.cullFace( gl.BACK );
+		// DEFAULT: The BACK FACE is culled!
 
 		// Enable DEPTH-TEST
-
 		gl.enable( gl.DEPTH_TEST );
 
 	} catch (e) {
@@ -887,17 +685,11 @@ function initWebGL( canvas ) {
 function runWebGL() {
 
 	var canvas = document.getElementById("my-canvas");
-
 	initWebGL( canvas );
-
 	shaderProgram = initShaders( gl );
-
 	setEventListeners( canvas );
-
 	initBuffers();
-
-	tick();		// NEW --- A timer controls the rendering / animation
-
+	tick();		// Timer that controls the rendering / animation
 	outputInfos();
 }
 
